1e93110eba0f462ab0dce0cb83ee05d72dd4bfb5
Andrew Molchanov <andymolchanov@gmail.com>
2022-09-19 13:53:17 +0300

    init

---
+++ app/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Description of index
+ *
+ * Created by
+ * @author Andrii Molchanov <andymolchanov@gmail.com>
+ * Date 19 сент. 2022 г.
+ * @var type Description
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>TestFeatures</title>
+    </head>
+    <body>
+        <div>
+            <img src='/img/under-construction.gif' alt="under-construction"/>
+        </div>
+        <a href="/img/Molchanov_web_dev_CV.pdf" download>
+            <button>Download PDF</button>
+        </a>
+    </body>
+</html>
